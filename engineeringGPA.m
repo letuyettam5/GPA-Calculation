7bc8efe7cfb8a4125b6800fa1394a7a60161ab42
dorsaaj <96406308+dorsaaj@users.noreply.github.com>
2022-04-19 17:20:47 -0400

    Update engineeringGPA.m

--- engineeringGPA.m
+++ engineeringGPA.m
@@ -1,4 +1,7 @@
 function [letterGrade_engr,creditHr_engr]=engineeringGPA(courseName,courseNumber,letterGrade,creditHr)
+%engineeringGPA function is used to get the course name and its corresponding number with the credit hour and letter grade.
+%Input arguments:courseName,courseNumber,letterGrade,creditHr
+% switch is used to check if the course name match 'ENGR' and its corresponding number.
 
     switch courseName
             case "ENGR"
@@ -7,7 +10,7 @@ function [letterGrade_engr,creditHr_engr]=engineeringGPA(courseName,courseNumber
                         letterGrade_engr=letterGrade;
                         creditHr_engr=creditHr;
                 end
-     otherwise
+     otherwise % if the subject is not "ENGR" the function is going to return 0
             letterGrade_engr="A";
             creditHr_engr=0;
     end
